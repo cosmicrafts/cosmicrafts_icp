565dbdc21846669edfae69cf644f2fb5da042dd6
Omar Hernandez Salmeron <ohsalmeron@gmail.com>
2024-01-03 02:12:32 -0600

    Yarn-Vite config para IC WebSockets
    se cambio a version a node18 para compatibilidad
    antes era 21
    
    Se agregaron los plugins de IC Websockets
    El import de IC Websockets en yarn no existia, se hizo con NPM
    
    El config de Vite necesito una adaptacion especial para los Polyfills porque se manejan diferente que en Webpack

--- src/cosmicrafts_icp_frontend/src/main.jsx
+++ src/cosmicrafts_icp_frontend/src/main.jsx
@@ -1,4 +1,5 @@
 // src/main.jsx
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter } from 'react-router-dom';
--- src/cosmicrafts_icp_frontend/vite.config.js
+++ src/cosmicrafts_icp_frontend/vite.config.js
@@ -1,15 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'; // Use named import
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {},
+  plugins: [
+    react(),
+    nodePolyfills({
+      // Include specific polyfills if needed
+      // include: [],
+
+      // Exclude specific polyfills if needed
+      // exclude: [],
+
+      // Configure global polyfills
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+
+      // Override specific module polyfills
+      // overrides: {},
+
+      // Enable polyfilling `node:` protocol imports
+      protocolImports: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      'crypto': 'crypto-browserify',
+    },
+  },
   build: {
-    outDir: '../../dist/cosmicrafts_icp_frontend', // Adjust this path as needed
+    outDir: '../../dist/cosmicrafts_icp_frontend',
   },
   esbuild: {
     jsxFactory: 'React.createElement',
     jsxFragment: 'React.Fragment',
   },
-})
+});
